005c95e8e4b467307e194e6edff2346a49b9cf3f
Nathi33 <nathi33.drnt@outlook.com>
2025-09-16 09:53:58 +0200

    add seo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'fr'
+language = 'en'
 
 # -- Options for HTML  --------------------------------------------
 html_theme = 'sphinx_rtd_theme'
