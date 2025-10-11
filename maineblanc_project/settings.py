91c9735e11acb4e499360d39fecc4f4e278df8db
Nathi33 <nathi33.drnt@outlook.com>
2025-10-11 12:30:36 +0200

    Add Render Database

--- maineblanc_project/settings.py
+++ maineblanc_project/settings.py
@@ -96,12 +96,20 @@ WSGI_APPLICATION = 'maineblanc_project.wsgi.application'
 # ============================================
 # DATABASE
 # ============================================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("RENDER") == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, "db_render.sqlite3"),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # ============================================
 # PASSWORD VALIDATION
